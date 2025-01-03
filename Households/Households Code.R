419aeed58bab948cc94d7c61d36d33e77f4da8ac
Andrew Saul <andrew.saul@phs.scot>
2025-01-03 11:41:32 +0000

    update: line 151 code : calculation of latest housing year

--- Households/Households Code.R
+++ Households/Households Code.R
@@ -148,7 +148,7 @@ houses_ts <- ggplot(house_dat1, aes(x = year, y = total_dwellings, group = 1)) +
   scale_y_continuous(labels = scales::comma, limits = c(0, 1.1 * max(house_dat1$total_dwellings))) +
   labs(
     x = "Year", y = "Number of Dwellings",
-    title = paste0("Number of Dwellings by Year in ", str_wrap(`LOCALITY`, 40)," ", max_year_housing),
+    title = paste0("Number of Dwellings by Year in ", str_wrap(`LOCALITY`, 40)," ", max(house_dat1$year)),
     caption = "Source: Council Tax billing system (via NRS)"
   ) +
   theme(plot.title = element_text(size = 12))
